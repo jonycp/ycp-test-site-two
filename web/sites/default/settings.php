c976fb294baa007a0839e0d1eb0a1a488a5748b9
jonycp <jshaw@ycp.edu>
2024-01-27 16:56:22 -0500

    committing settings.php database lines moved to settings.local.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,16 +32,4 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-$databases['default']['default'] = array (
-  'database' => 'pantheon3',
-  'username' => 'jonycp',
-  'password' => 'p4nth3onjon',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
 $settings['hash_salt'] = '4PFsQ-ZXEdiiH6lGV5KSAbx1oBOnvR9WNupTPB8iafQGSuOQn0Y--5s9zBmLJiPRuqzb4shQBQ';
